91f684034850f28c9f72f98adc289bb6e35d8b13
daniel natale <dnatale667@gmail.com>
2021-10-22 13:02:01 -0400

    aiwon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 
 const promptUser = () => {
@@ -55,6 +55,9 @@ const promptUser = () => {
       }
     ]);
   };
+
+
+  
   const promptProject = portfolioData => {
     if(!portfolioData.projects){
     portfolioData.projects = [];
@@ -120,15 +123,14 @@ const promptUser = () => {
   
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData)});
+const pageHTML = generatePage(portfolioData);
+// console.log(portfolioData)
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+});
 
 
 
